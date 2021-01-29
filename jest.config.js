8f9a44831e7278b17135acdbfa5398b702cc734f
Kevin Ramharak <kevin@ramharak.nl>
2021-01-29 22:44:11 +0100

    fix being able to build and test the package

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    transform: {
-        "^.+\\.tsx?$": "ts-jest"
-    },
-    testRegex: "test/.*\\.test\\.ts$",
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-};
